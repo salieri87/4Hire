c5a6cec25b45c2230a216ee139fcddb8a528bc58
Filip Chwastowski <chwastek@gmail.com>
2020-01-27 23:38:52 +0100

    View model holds table view delegate and data source methods.

--- 4Hire/4Hire/ViewController.swift
+++ 4Hire/4Hire/ViewController.swift
@@ -9,11 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     var viewModel = CandidateViewModel.mock()
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = viewModel.name
+        tableView.delegate = viewModel
+        tableView.dataSource = viewModel
+        tableView.reloadData()
+    }
+}
+
+struct Layout {
+    enum Section: Int {
+        case experience
+        case faculties
+    }
+}
+
+extension CandidateViewModel: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case Layout.Section.experience.rawValue:
+            return experience.count
+        case Layout.Section.faculties.rawValue:
+            return education.count
+        default:
+            return 0
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        switch indexPath.section {
+            case Layout.Section.experience.rawValue:
+                cell.textLabel?.text = "Position"
+                cell.detailTextLabel?.text = experience[indexPath.row].name
+            case Layout.Section.faculties.rawValue:
+                cell.textLabel?.text = "School"
+                cell.detailTextLabel?.text = education[indexPath.row].school
+            default:
+                assert(false)
+            }
+    
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case Layout.Section.experience.rawValue:
+            return "Experience"
+        case Layout.Section.faculties.rawValue:
+            return "Education"
+        default:
+            return nil
+        }
     }
 }
