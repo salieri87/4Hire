03a9aeaf11a1d76790f85809ea71de13abe5c1a6
Filip Chwastowski <chwastek@gmail.com>
2020-01-27 22:10:59 +0100

    Introduced models for working and school experiences.

--- 4Hire/4Hire/Candidate.swift
+++ 4Hire/4Hire/Candidate.swift
@@ -13,16 +13,31 @@ struct Candidate: Decodable, Equatable {
     var identifier: Int
     var firstName: String
     var lastName: String
-    
-    enum CodingKeys: String, CodingKey {
-        case identifier
-        case firstName
-        case lastName
-    }
+    /// List of past and current roles.
+    var experience: [Position]?
+    /// List of past and current studies.
+    var education: [Education]?
     
     static func == (lhs: Self, rhs: Self) -> Bool {
         return lhs.identifier == rhs.identifier &&
             lhs.firstName == rhs.firstName &&
             lhs.lastName == rhs.lastName
     }
+}
+
+/// Wrapper for job position.
+struct Position: Decodable {
+    var name: String
+    var company: String
+    var startedAt: Date
+    var finishedAt: Date?
+    var jobDescription: String
+}
+
+/// Wrapper for education background.
+struct Education: Decodable {
+    var school: String
+    var startedAt: String
+    var finishedAt: String?
+    var course: String
 }
