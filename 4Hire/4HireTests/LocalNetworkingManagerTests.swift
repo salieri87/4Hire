7085477f80a4fd035f82b370acdd9ceccab76aa6
Filip Chwastowski <chwastek@gmail.com>
2020-01-28 20:54:40 +0100

    Added failing test for fetching malformed data.

--- 4Hire/4HireTests/LocalNetworkingManagerTests.swift
+++ 4Hire/4HireTests/LocalNetworkingManagerTests.swift
@@ -13,7 +13,7 @@ class LocalNetworkingManagerTests: XCTestCase {
     
     let networkingResponseTimeout = 0.1
 
-    func test_requestData_returnsExpectedData() {
+    func test_requestData_fromExistingResource_returnsExpectedData() {
         // Given
         let manager = LocalNetworkingManager.sharedManager
         guard let url = Bundle(for: LocalNetworkingManagerTests.self).url(forResource: "candidate_test", withExtension: "json") else {
@@ -28,6 +28,28 @@ class LocalNetworkingManagerTests: XCTestCase {
                 // Then
                 let candidate = try! JSONDecoder().decode(Candidate.self, from: model)
                 XCTAssertEqual(candidate, self.stubModel())
+                expectation.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        waitForExpectations(timeout: networkingResponseTimeout, handler: nil)
+    }
+    
+    func test_requestData_fromMalformedResource_failsWithError() {
+        // Given
+        let manager = LocalNetworkingManager.sharedManager
+        guard let url = Bundle(for: LocalNetworkingManagerTests.self).url(forResource: "candidate_malformed", withExtension: "json") else {
+            XCTFail("Seems like test resource is missing.")
+            return
+        }
+        
+        // When
+        let expectation = self.expectation(description: "Manager should fail when fetching malformed data.")
+        manager.requestData(from: url) { (result) in
+            if case let .failure(error) = result {
+                // Then
+                XCTAssertEqual(error, .parsingFailed)
                 expectation.fulfill()
             } else {
                 XCTFail()
