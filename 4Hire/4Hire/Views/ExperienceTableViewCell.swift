ef46d1e29752fe6cb33d77f16cd1fe808bbf0a9b
Filip Chwastowski <chwastek@gmail.com>
2020-01-29 03:10:04 +0100

    Imported icon for a job position.

---
+++ 4Hire/4Hire/Views/ExperienceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExperienceTableViewCell.swift
+//  4Hire
+//
+//  Created by Filip Chwastowski on 29/01/2020.
+//  Copyright © 2020 Home. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
