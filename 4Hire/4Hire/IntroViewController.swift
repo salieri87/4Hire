1b891ac4691ea50c91db085648eb59b4d97e30a5
Filip Chwastowski <chwastek@gmail.com>
2020-01-30 02:12:17 +0100

    Animation (sliding in) of avatar's autolayout.

--- 4Hire/4Hire/IntroViewController.swift
+++ 4Hire/4Hire/IntroViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class IntroViewController: UIViewController {
-    @IBOutlet var avatarView: UIImageView!
-    @IBOutlet var nextButton: UIButton!
-    @IBOutlet var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var avatarView: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var avatarYContstraint: NSLayoutConstraint!
 
     private var fetcher = GithubAvatarFetcher()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        avatarYContstraint.constant = view.bounds.height
+        avatarView.layer.cornerRadius = avatarView.bounds.width / 2.0
+        view.layoutSubviews()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,10 +36,25 @@ class IntroViewController: UIViewController {
                 self.spinner.stopAnimating()
                 if case let .success(data) = result {
                     self.avatarView.image = UIImage(data: data)
+                    self.slideInAvatar()
                 } else {
                     print("failed fetching avatar")
                 }
             }
+        }
+    }
+    
+    func slideInAvatar() {
+        avatarYContstraint.constant = 0
+        UIView.animate(withDuration: 1.0,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.4,
+                       initialSpringVelocity: 0.3,
+                       options: .curveEaseInOut,
+                       animations: {
+            self.view.layoutSubviews()
+        }) { (_) in
+            self.nextButton.isHidden = false
         }
     }
 }
